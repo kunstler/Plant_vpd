08e68f39a0612ac079428c3f0d0bf5d66e5bc421
kunstler <georges.kunstler@gmail.com>
2016-04-07 17:49:03 +0200

    get wc climate from gbif species ons to match zanne data

--- R/climate.R
+++ R/climate.R
@@ -1,88 +1,32 @@
-#Returns the index of the nearest plot(x2,y2) to each of the target plots (x1,y1)
-NearestPlot <- function(x1,y1,x2,y2) {
-
-  res = numeric(length(x1))
-
-  for (i in 1:length(x1)) {
-    dists = sqrt((x1[i] - x2)^2 + (y1[i] - y2)^2)
-    res[i] = which.min(dists)
-    }
-  return(res)
-
-} #NearestPlot
 
 
 #looks up MAT and MAP for given lat/lon values
-GetClimate <-function(lats,lons) {
-  #create raster grid for tiles
-  xNum<-0:11
-  yNum<-0:4
+GetClimate <-function(lats,lons, wc) {
+  plot.mat <- numeric(length(lats))
+  plot.tmax <- numeric(length(lats))
+  plot.map <- numeric(length(lats))
+  plot.pdry <- numeric(length(lats))
 
-  xLon<-seq(from=-165,to=165,by=30)
-  yLat<-seq(from=75,to=-45,by=-30)
-
-  NumMat<-outer(yNum,xNum,paste,sep="")
-  NumLookup<-as.vector(NumMat)
-
-  NumRast <- raster(matrix(1:60,nrow=5),xmn=-180,xmx=180,ymn=-60,ymx=90)
-
-  LonLatLookup<-expand.grid(Lat=yLat,Lon=xLon)
-  row.names(LonLatLookup) <- NumMat
-
-  #get the set of tiles corresponding to the lons/lats arguments
-  tiles <- extract(NumRast,cbind(lons,lats))
-  unique.tiles <- unique(tiles)
-
-  plot.mat <- numeric(length(tiles))
-  plot.tmax <- numeric(length(tiles))
-  plot.map <- numeric(length(tiles))
-  plot.pdry <- numeric(length(tiles))
-  #download each of the tiles
-  for (i.tile in unique.tiles) {
-    temp.tile <- tryCatch(getData('worldclim',
-                                  var = 'bio',
-                                  res = 0.5,
-                                  lon = LonLatLookup[i.tile,"Lon"],
-                                  lat = LonLatLookup[i.tile,"Lat"]),
-                          error = function(err) NA)
-
-  if(class(temp.tile) == "RasterStack"){
-
-    plot.mat[tiles==i.tile] <- extract(temp.tile,
-                                       cbind(lons[tiles==i.tile],
-                                             lats[tiles==i.tile]),
-                                       layer=1,nl=1)/10
-                                  #retrieve MAT, divide by 10 for deg C
-    plot.tmax[tiles==i.tile] <- extract(temp.tile,
-                                       cbind(lons[tiles==i.tile],
-                                             lats[tiles==i.tile]),
-                                       layer=5,nl=1)/10
-                                  #retrieve T max of warmest month, divide by 10 for deg C
-    plot.map[tiles==i.tile] <- extract(temp.tile,
-                                       cbind(lons[tiles==i.tile],
-                                             lats[tiles==i.tile]),
-                                       layer=12,nl=1) #retrieve MAP
-    plot.pdry[tiles==i.tile] <- extract(temp.tile,
-                                       cbind(lons[tiles==i.tile],
-                                             lats[tiles==i.tile]),
-                                       layer=14,nl=1) #retrieve P dryest month
-    nas <- which(tiles==i.tile & is.na(plot.mat))
-
-    #assign climate of nearest plot to all nas
-    if (length(nas)>0 & sum(!is.na(plot.mat))>3) {
-      good <- which(tiles==i.tile & !is.na(plot.mat))
-
-      near <- NearestPlot(lons[nas],lats[nas],lons[good],lats[good])
-
-      plot.mat[nas] <- plot.mat[good[near]]
-      plot.map[nas] <- plot.map[good[near]]
-      plot.tmax[nas] <- plot.tmax[good[near]]
-      plot.pdry[nas] <- plot.pdry[good[near]]
-    }
-   }
-  }
-
-
+  plot.mat <- extract(wc,
+                      cbind(lons,
+                            lats),
+                      layer=1,nl=1)/10
+  #retrieve MAT, divide by 10 for deg C
+  plot.tmax <- extract(wc,
+                       cbind(lons,
+                             lats),
+                       layer=2,nl=1)/10
+  #retrieve T max of warmest month, divide by 10 for deg C
+  plot.map <- extract(wc,
+                      cbind(lons,
+                            lats),
+                      layer=3,nl=1)
+  #retrieve MAP
+  plot.pdry <- extract(wc,
+                       cbind(lons,
+                             lats),
+                       layer=4,nl=1)
+  #retrieve P dryest month
   return(data.frame(MAT=plot.mat,MAP=plot.map, TMAX = plot.tmax, PDRY = plot.pdry))
 
 } #GetClimate
--- R/zanne.R
+++ R/zanne.R
@@ -18,7 +18,7 @@ process_zanne_2010<- function(filename){
 
 ## get climatic niche of the species
 
-get_climate_niche_sp<- function(sp){
+get_climate_niche_sp<- function(sp, wc){
 require(dismo)
 require(rgbif)
 ##data cleaning TODO IMPROVE JUST A FIRST GUESS
@@ -30,37 +30,89 @@ dat <- tryCatch(occ_search(taxonKey=key, return='data',
                 error = function(err) NA)
 if(is.data.frame(dat)){
     dat <-  dat %>% occ_issues(-cdout, -cdrepf, -cucdmis, -preswcd, -zerocd)
+    dat <- dat[!is.na(dat$decimalLatitude) & dat$decimalLatitude != 0 &
+               !is.na(dat$decimalLongitude) & dat$decimalLongitude != 0, ]
 
-    if(nrow(dat)>0){
+   if(nrow(dat)>0){
       #gbifmap(dat)
-      dat <- dat[!is.na(dat$decimalLatitude) & dat$decimalLatitude != 0 &
-                 !is.na(dat$decimalLongitude) & dat$decimalLongitude != 0, ]
-      df_clim <- GetClimate(dat$decimalLatitude, dat$decimalLongitude)
-
+      df_clim <- GetClimate(dat$decimalLatitude, dat$decimalLongitude, wc)
+      if(nrow(df_clim)>0){
+        df_wc <- lapply(df_clim, mean, na.rm = TRUE)
         res <- data.frame(Binomial= sp,
-                          do.call("cbind", lapply(df_clim, mean)),
-                          nobs = nrow(dat))
-      print(paste("species ", sp, " done"))
-    }else{
-      res  <- data.frame(Binomial= sp,
-                         MAT = NA, MAP = NA, TMAX = NA, PDRY = NA,
-                         nobs = nrow(dat))
-      print(paste("species ", sp, " error wc"))
-    }
-}else{
+                          do.call("cbind", df_wc),
+                          nobs = nrow(dat), stringsAsFactors = FALSE)
+        names(res) <-  c("Binomial", "MAT", "MAP", "TMAX", "PDRY", "nobs")
+        print(paste("species ", sp, " done"))
+      }else{
+        res  <- data.frame(Binomial= sp,
+                           MAT = NA, MAP = NA, TMAX = NA, PDRY = NA,
+                           nobs = 0, stringsAsFactors = FALSE)
+        print(paste("species ", sp, " error in wc no data with climate"))
+     }
+   }else{
     res  <- data.frame(Binomial= sp,
                        MAT = NA, MAP = NA, TMAX = NA, PDRY = NA,
-                       nobs = nrow(dat))
-    print(paste("species ", sp, " errro gbif"))
+                       nobs = 0, stringsAsFactors = FALSE)
+    print(paste("species ", sp, " error in gbif no data with coordinates"))
+   }
+}else{
+res  <- data.frame(Binomial= sp,
+                   MAT = NA, MAP = NA, TMAX = NA, PDRY = NA,
+                   nobs = 0, stringsAsFactors = FALSE)
+print(paste("species ", sp, " error in gbif no data"))
 }
 return(res)
 }
 
-get_climate_niche<- function(df){
+get_wc <- function(dir = "wc0_5"){
+require(gtools)
+require(raster)
+list_ras <- mixedsort(list.files(dir,
+                                 full.names = TRUE,
+                                 pattern = ".bil"))
+wc <- raster::stack(list_ras)
+return(wc)
+}
+
+get_climate_niche<- function(df, wc){
 require(dplyr)
-res <- rbind_list(lapply(df$Binomial, get_climate_niche_sp))
+require(parallel)
+cl <- makeCluster(20)
+list_res <- parLapply(cl, df$Binomial, get_climate_niche_sp, wc = wc)
+res <- dplyr::rbind_all(list_res)
+stopCluster(cl)
 rr <- left_join(df, res, by = "Binomial")
 return(rr)
 }
+##
 
 
+plot_sma <- function(df, var_x, var_y, log="xy"){
+  sm1 <- sma(df[[var_y]] ~ df[[var_x]], log=log)
+  par(mar=c(4.6, 4.6, .5, .5))
+  plot(NA, type="n", log=log, xlim=range(df[[var_x]], na.rm = TRUE),
+       ylim= range(df[[var_y]], na.rm = TRUE),
+       xlab="", ylab="", las=1)
+  mtext(var_x, line=3, side = 1)
+  mtext(var_y, line=3, side = 2)
+  points(df[[var_x]], df[[var_y]], col=make_transparent("grey", 0.5), pch=16)
+  plot(sm1, add=TRUE, type="l", lwd=1.25, p.lines.transparent=NA)
+}
+
+plot_wood_clim <- function(df){
+require(smatr)
+df <-  df[!is.na(df$MAP), ]
+df$PDRY <- df$PDRY + 0.006
+df$P_T <- df$PDRY/df$MAT
+
+par(mfrow = c(2,3))
+plot_sma(df, var_x = "MAP", var_y = "A_mm_2")
+plot_sma(df, var_x = "MAP", var_y = "F_mm_2_mm_2")
+plot_sma(df, var_x = "MAP", var_y = "N_mm_2")
+
+plot_sma(df, var_x = "PDRY", var_y = "A_mm_2")
+plot_sma(df, var_x = "PDRY", var_y = "F_mm_2_mm_2")
+plot_sma(df, var_x = "PDRY", var_y = "N_mm_2")
+
+
+}
