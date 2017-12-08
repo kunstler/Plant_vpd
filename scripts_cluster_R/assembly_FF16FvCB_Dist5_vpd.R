15a5e21ace182efefb3ee9828312f7865d67e643
kunstler <georges.kunstler@gmail.com>
2017-12-08 14:38:14 +0100

    progress on setting up cluster

---
+++ scripts_cluster_R/assembly_FF16FvCB_Dist5_vpd.R
@@ -0,0 +1,11 @@
+# Script for cluster to run assembly with FF16FvCB DIST 5
+library(plant)
+library(plant.assembly)
+library(loggr)
+
+source("R/assembly.R")
+
+vpd <- commandArgs(trailingOnly=TRUE)/10
+res <- run_assembly_FvCB_narea_lma(vpd = vpd, disturbance_mean_interval = 5)
+saveRDS(res, file = file.path("output_cluster", paste0("FvCB_lma_Narea_dist5_vpd", , ".rds")))
+
---
+++ scripts_cluster_shell/run_R_FF16FvCB_Dist5.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#SBATCH --job-name=run_R_vpd
+#SBATCH -p q-irstea
+#SBATCH --time=00:05:00                                    
+#SBATCH -A u_emgr                                          
+#SBATCH -o /scratch/gerard.henry/%j.out
+#SBATCH -e /scratch/gerard.henry/%j.err
+#SBATCH --mail-type=ALL                                   
+#SBATCH --mail-user=georges.kunstler@irstea.fr                                                                                                    
+#SBATCH --mem=80000
+  
+module unload gcc
+module load R/gcc
+module load lapack/gcc/64/3.8.0-with-blas
+module load nlopt/2.4.2                                                         
+
+echo ${SLURM_JOB_NODELIST}
+echo ${SLURM_ARRAY_TASK_ID}
+
+
+# on lance le script R en precisant bien son chemin 
+R CMD BATCH "--args ${SLURM_ARRAY_TASK_ID}" scripts_cluster/assembly_FF16FvCB_Dist5_vpd.R
