646eb0b5e61c9654929e226b800859779670499f
kunstler <georges.kunstler@gmail.com>
2018-01-22 15:27:46 +0100

    fix error script R cluster

--- scripts_cluster_R/assembly_FF16FvCB_Tleaf_Dist40_vpd.R
+++ scripts_cluster_R/assembly_FF16FvCB_Tleaf_Dist40_vpd.R
@@ -15,7 +15,7 @@ source("R/assembly.R")
 dir.create("output_cluster", showWarnings = FALSE)
 vpd <- vpd/10
 print( vpd)
-res <- run_assembly_FvCB_narea_lma <- Tleaf(vpd = vpd, name_param_Tleaf = "output/coef_T_vpd.rds",
+res <- run_assembly_FvCB_narea_lma_Tleaf(vpd = vpd, name_param_Tleaf = "output/coef_T_vpd.rds",
                                             disturbance_mean_interval = 40)
 saveRDS(res, file = file.path("output_cluster", paste0("FvCB_lma_Narea_Tleaf_dist40_vpd", vpd, ".rds")))
 
