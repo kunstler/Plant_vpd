f8474ead80d10f321e7d3f55d3a0e52d8d0dff4f
kunstler <georges.kunstler@gmail.com>
2018-01-22 11:38:29 +0100

    add shell script for new simul Tleaf

---
+++ scripts_cluster_R/assembly_FF16FvCB_Tleaf_Dist5_vpd.R
@@ -0,0 +1,21 @@
+# Script for cluster to run assembly with FF16FvCB Tleaf DIST 5
+library(plant)
+library(plant.assembly)
+library(loggr)
+
+args <- commandArgs(trailingOnly=TRUE)
+if(length(args)==0){
+    print("No arguments supplied.")
+}else{
+    for(i in 1:length(args)){
+         eval(parse(text=args[[i]]))
+    }
+}
+
+source("R/assembly.R")
+dir.create("output_cluster", showWarnings = FALSE)
+vpd <- vpd/10
+print( vpd)
+res <- run_assembly_FvCB_narea_lma_Tleaf(vpd = vpd, name_param_Tleaf = "output/coef_T_vpd.rds",
+                                         disturbance_mean_interval = 5)
+saveRDS(res, file = file.path("output_cluster", paste0("FvCB_lma_Narea_Tleaf_dist5_vpd",vpd , ".rds")))
